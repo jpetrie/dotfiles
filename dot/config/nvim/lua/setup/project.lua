42719abdcdfcb8de5103a61573a49cf76f6ff518
Josh Petrie <josh@joshpetrie.net>
2025-02-12 17:57:25 -0800

    Use Schematic hooks to trigger Overseer.

--- dot/config/nvim/lua/setup/project.lua
+++ dot/config/nvim/lua/setup/project.lua
@@ -1,12 +1,51 @@
+local function register_task_template(task, name)
+  local overseer = require("overseer")
+  local schematic = require("schematic")
+  overseer.register_template({
+    name = task,
+    builder = function()
+      local project = schematic.project()
+      if project ~= nil then
+        return {
+          name = name .. " (" .. project.target.name .. "/" .. project.config.name .. ")",
+          cmd = project.target.tasks[task],
+          components = {
+            "default",
+            {"on_output_quickfix", open_on_match = true, close = true, items_only = true},
+          },
+        }
+      end
+      return nil
+    end
+  })
+end
+
 return {
   {
     "jpetrie/schematic",
     config = function()
+      local overseer = require("overseer")
       local schematic = require("schematic")
       schematic.setup({
-        use_task_runner = "overseer",
+        hooks = {
+          clean = function(_)
+            overseer.run_template({name = "clean"})
+          end,
+          build = function(_)
+            overseer.run_template({name = "build"})
+          end,
+          run = function(_)
+            overseer.run_template({name = "run"})
+          end,
+        }
       })
 
+      -- Create Overseer task templates for clean, build and run actions.
+      register_task_template("clean", "Clean")
+      register_task_template("build", "Build")
+      register_task_template("run", "Run")
+
+      -- Setup key mappings.
       vim.keymap.set("n", "<LEADER>k", function()
         local project = schematic.project()
         if project ~= nil then
