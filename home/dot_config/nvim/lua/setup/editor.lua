55160849111ce9f395d6c688ffe42421e71fc7f8
Josh Petrie <josh@joshpetrie.net>
2025-05-17 16:32:23 -0700

    Automatically update .spl files on startup.

--- home/dot_config/nvim/lua/setup/editor.lua
+++ home/dot_config/nvim/lua/setup/editor.lua
@@ -25,6 +25,21 @@ vim.opt.wrap = false
 vim.opt.spell = true
 vim.opt.spellfile = vim.fs.abspath("~/.config/nvim/spell/custom.utf-8.add")
 
+-- On startup, if any .add files are newer than their .spl files, update them.
+vim.api.nvim_create_autocmd("VimEnter", {pattern = "*", callback = function()
+  for _, add_file in ipairs(vim.opt.spellfile:get()) do
+    local spl_file = add_file .. ".spl"
+    if vim.fn.filereadable(add_file) == 1 then
+      local add_time = vim.fn.getftime(add_file)
+      local spl_time = vim.fn.getftime(spl_file)
+      if add_time > spl_time or spl_time == -1 then
+        vim.cmd("silent! mkspell! " .. spl_file .. " " .. add_file)
+      end
+    end
+  end
+end
+})
+
 return {
   {
     "nvim-lua/plenary.nvim",
