2f78c716f836609e26d2920b9c7a66064e3ebb8e
Josh Petrie <josh@joshpetrie.net>
2025-12-18 20:37:00 -0800

    Use submodules and Neovim packages for plugins.

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,13 +1,3 @@
--- Ensure Lazy is installed.
-local lazy = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazy) then
-  local repository = "https://github.com/folke/lazy.nvim.git"
-  local output = vim.fn.system({"git", "clone", "--filter=blob:none", "--branch=stable", repository, lazy})
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({{"Failed to clone lazy.nvim:\n" .. output, "ErrorMsg"}}, true, {})
-  end
-end
-
 if vim.fn.has("mac") ~= 0 then
   -- Prevent flicker on launch (see https://github.com/neovim/neovim/issues/19362).
   vim.fn.system("defaults read -g AppleInterfaceStyle")
@@ -23,6 +13,20 @@ end
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Appearance and UI
+vim.opt.colorcolumn = "+1"
+vim.opt.list = true
+vim.opt.listchars = "eol:¬,tab:>-,space:·,extends:▶,precedes:◀,nbsp:•"
+vim.opt.number = true
+vim.opt.ruler = false
+vim.opt.shortmess = "filmrxWIF"
+vim.opt.showcmd = false
+vim.opt.signcolumn = "yes:1"
+vim.opt.wildmode = "longest:full"
+
+-- Completion
+vim.opt.completeopt = {"menuone", "noinsert"}
 
 -- Files and Paths
 vim.opt.wildignore = {"*.air", "*.d", "*.dia", "*.o"}
@@ -45,17 +49,6 @@ vim.opt.linebreak = true
 vim.opt.showbreak="↳"
 vim.opt.wrap = false
 
--- Appearance and UI
-vim.opt.colorcolumn = "+1"
-vim.opt.list = true
-vim.opt.listchars = "eol:¬,tab:>-,space:·,extends:▶,precedes:◀,nbsp:•"
-vim.opt.number = true
-vim.opt.ruler = false
-vim.opt.shortmess = "filmrxWIF"
-vim.opt.showcmd = false
-vim.opt.signcolumn = "yes:1"
-vim.opt.wildmode = "longest:full"
-
 -- Search
 vim.opt.hlsearch = false
 
@@ -75,12 +68,35 @@ vim.opt.statusline = "%!v:lua.BuildStatusLine()"
 -- Edit init.lua.
 vim.keymap.set("n", "ev", ":e $MYVIMRC<CR>", {desc = "Edit init.lua"})
 
+vim.keymap.set("i", "<C-space>", "<C-y>", {desc = "Accept current completion"})
+vim.keymap.set("i", "<C-j>", "<C-n>", {desc = "Select next completion"})
+vim.keymap.set("i", "<C-k>", "<C-p>", {desc = "Select prior completion"})
+
 -- Jump to long lines.
 vim.keymap.set("n", "]ll", "/\\%>" .. vim.o.textwidth .. "v.\\+<CR>", {desc = "Jump to next long line"})
 vim.keymap.set("n", "[ll", "?\\%>" .. vim.o.textwidth .. "v.\\+<CR>", {desc = "Jump to prior long line"})
 
 -- Populate the quickfix list with TODO comments.
 vim.api.nvim_create_user_command("Todo", "silent grep! TODO: | copen", {})
+
+-- Flip between file counterparts.
+vim.keymap.set("n", "<LEADER>a", ":Flip next<CR>", {desc = "Flip to the next counterpart"})
+
+-- Invoke Lantern tasks.
+vim.keymap.set("n", "<LEADER>k", function() require("lantern").clean() end, {desc = "Clean the current Lantern target"})
+vim.keymap.set("n", "<LEADER>b", function() require("lantern").build() end, {desc = "Build the current Lantern target"})
+vim.keymap.set("n", "<LEADER>r", function() require("lantern").run() end, {desc = "Run the current Lantern target"})
+
+-- Launch Telescope.
+vim.keymap.set("n", "<LEADER>of", ":Telescope find_files<CR>", {desc = "File files"})
+vim.keymap.set("n", "<LEADER>ob", ":Telescope buffers<CR>", {desc = "Find buffers"})
+vim.keymap.set("n", "<LEADER>fg", ":Telescope live_grep<CR>", {desc = "Find in files"})
+vim.keymap.set("n", "<LEADER>fh", ":Telescope help_tags<CR>", {desc = "Find in help"})
+
+-- Session management.
+vim.keymap.set("n", "<leader>ss", require("resession").save)
+vim.keymap.set("n", "<leader>sl", require("resession").load)
+vim.keymap.set("n", "<leader>sd", require("resession").delete)
 
 
 -- =====================================================================================================================
@@ -202,6 +218,8 @@ vim.api.nvim_create_autocmd({"FileType"}, {pattern = "cpp", callback = function(
       root_dir = lantern.project().directory,
     })
   end
+
+  vim.treesitter.start()
 end})
 
 
@@ -224,264 +242,71 @@ vim.diagnostic.config({
 
 
 -- =====================================================================================================================
--- Plugins
+-- Plugin Setup
 
-if not vim.uv.fs_stat(lazy) then
-  -- Nothing but plugin configuration should be below this point in the file, so early-out here if Lazy is not
-  -- available. This reduces the overall nesting of the plugin specifications.
-  return
-end
-
-vim.opt.runtimepath:prepend(lazy)
-require("lazy").setup({
-  dev = {path = "~/Developer", fallback = true},
-  spec = {
-    {
-      "bezhermoso/tree-sitter-ghostty",
-      build = "make nvim_install",
-    },
-    {
-      "folke/snacks.nvim",
-      priority = 1000,
-      opts = function(_, options)
-        options.dashboard = {
-          enabled = true,
-          width = 60,
-          sections = {
-            {title = "Neovim " .. tostring(vim.version()), padding = 2, align = "center"},
-            function()
-              local has_resession, resession = pcall(require, "resession")
-              local results = {}
-              if has_resession then
-                table.insert(results, {text = {"Recent Sessions: ───────────────────────────────────────────", hl = "SnacksDashboardKey"}})
-                for index, name in ipairs(resession.list()) do
-                  local key = tostring(index)
-                  table.insert(results, {
-                    text = {{name, width = 53, align = "left"}, {"[" .. key .. "] ", align = "right", hl = "SnacksDashboardDir"}},
-                    action = function() resession.load(name) end,
-                    key = key,
-                    indent = 2,
-                  })
-                end
-              end
-
-              return results
-            end,
-          },
-        }
-        
-        options.indent = {
-          enabled = true,
-          only_scope = true,
-          animate = {enabled = false},
-          filter = function (buffer)
-            local ignore_filetypes = {"help", "markdown", "text"}
-            return not vim.list_contains(ignore_filetypes, vim.bo[buffer].filetype)
-          end,
-        }
-
-        return options
-      end,
-    },
-    {
-      "jpetrie/flip",
-      tag = "1.0.0",
-      opts = {},
-      keys = {
-        {"<LEADER>a", ":Flip next<CR>", desc = "Flip to the next counterpart"},
-      }
-    },
-    {
-      "jpetrie/lantern",
-      lazy = false,
-      config = function()
-        local lantern = require("lantern")
-        lantern.setup({
-          exclude_binary_directory_patterns = {"Xcode"},
-        })
-
-        vim.api.nvim_create_autocmd("VimEnter", { callback = function(_) lantern.scan() end })
-      end,
-      keys = {
-        {
-          "<LEADER>k",
-          function()
-            vim.cmd("wa")
-            require("lantern").clean()
-          end,
-          desc = "Clean the active target"
-        },
-        {
-          "<LEADER>b",
-          function()
-            vim.cmd("wa")
-            require("lantern").build()
-          end,
-          desc = "Build the active target"
-        },
-        {
-          "<LEADER>r",
-          function()
-            vim.cmd("wa")
-            require("lantern").run()
-          end,
-          desc = "Run the active target"
-        },
-      },
-    },
-    {
-      "jpetrie/lightswitch",
-      opts = {},
-    },
-    {
-      "jpetrie/turnip",
-      priority = 1000,
-      config = function()
-        require("turnip").setup({
-          custom_groups = {
-            SnacksIndent = {fg = "ui_invisible"},
-            SnacksIndentScope = {fg = "green_faint"},
-          }
-        })
-
-        vim.cmd("colorscheme turnip")
-      end,
-    },
-    {
-      "ngynkvn/gotmpl.nvim",
-      opts = {},
-    },
-    {
-      "nvim-telescope/telescope.nvim",
-      opts = {
-        layout_strategy = "vertical",
-        layout_config = {height = 0.95}
-      },
-      keys = {
-        {"<LEADER>of", ":Telescope find_files<CR>", desc = "File files"},
-        {"<LEADER>ob", ":Telescope buffers<CR>", desc = "Find buffers"},
-        {"<LEADER>fg", ":Telescope live_grep<CR>", desc = "Find in files"},
-        {"<LEADER>fh", ":Telescope help_tags<CR>", desc = "Find in help"},
-      },
-    },
-    {
-      "nvim-telescope/telescope-fzy-native.nvim",
-      opts = function(_, options)
-        require("telescope").load_extension("fzy_native")
-        return options
-      end,
-    },
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      opts = {
-        ensure_installed = {"c", "cmake", "cpp", "gitignore", "gotmpl", "json", "query", "rust", "lua", "vim", "vimdoc"},
-        sync_install = false,
-        auto_install = false,
-        indent = {
-          enable = false,
-        },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-      },
-    },
-    {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      main = "nvim-treesitter.configs",
-      opts = {
-        textobjects = {
-          select = {
-            enable = true,
-            keymaps = {
-              ["i,"] = "@parameter.inner",
-              ["a,"] = "@parameter.outer",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["],"] = "@parameter.outer",
-            },
-            goto_previous_start = {
-              ["[,"] = "@parameter.outer",
-            },
-          },
-        },
-      },
-    },
-    {
-      "saghen/blink.cmp",
-      tag = "v1.7.0",
-      opts = {
-        keymap = {
-          preset = "none",
-          ["<CR>"] = {"accept", "fallback"},
-          ["<C-j>"] = {"select_next", "fallback"},
-          ["<C-k>"] = {"select_prev", "fallback"},
-        },
-      },
-    },
-    {
-      "stevearc/overseer.nvim",
-      opts = {},
-      keys = {
-        {"<LEADER>tl", function() require("overseer").toggle({direction = "bottom"}) end, desc = "Toggle task list"},
-      },
-    },
-    {
-      "stevearc/resession.nvim",
-      opts = function(_, options)
-        SessionSave = function(behavior)
-          local default_behavior = {prompt = true}
-          behavior = vim.tbl_extend("keep", behavior or {}, default_behavior)
-
-          local has_resession, resession = pcall(require, "resession")
-          if has_resession then
-            local info = resession.get_current_session_info()
-            if info ~= nil then
-              resession.save(info.name)
-            elseif behavior ~= nil and behavior.prompt then
-              local default_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-              local session_name = vim.fn.input("Save session as: ", default_name)
-              if #session_name > 0 then
-                resession.save(session_name)
-              end
-            end
-          end
-        end
-
-        options.buf_filter = function(buffer)
-          -- Do not save the filetypes in this list to the session.
-          local skip = {"gitcommit", "help"}
-          if vim.list_contains(skip, vim.bo[buffer].filetype) then
-            return false
-          end
-
-          -- For everything not explicitly skipped, fall back to the default behavior.
-          return require("resession").default_buf_filter(buffer)
-        end
-
-        -- When quitting, save the active session if it exists.
-        vim.api.nvim_create_autocmd("VimLeavePre", {
-          callback = function()
-            SessionSave({prompt = false})
-          end,
-        })
-
-        return options
-      end,
-      keys = {
-        {"<LEADER>ss", function() SessionSave() end, desc = "Save the current session"},
-        {"<LEADER>sl", function() require("resession").load() end, desc = "Load a session"},
-        {"<LEADER>sd", function() require("resession").delete() end, desc = "Delete a session"},
-      },
-    }
-  },
-  install = {
-    colorscheme = {"default"},
-  },
+require("lantern").setup({
+  exclude_binary_directory_patterns = {"Xcode"},
+  save_before_task = true,
 })
+
+require("nvim-treesitter").install({"c", "cpp", "doxygen"})
+
+require("mini.completion").setup({
+  lsp_completion = {
+    source_func = "omnifunc"
+  },
+  mappings = {
+    -- The default for this is <C-space>, which has been mapped to <C-y> earlier in this file to accept a completion.
+    force_twostep = ""
+  }
+})
+
+local icons = require("mini.icons")
+icons.setup()
+icons.mock_nvim_web_devicons()
+icons.tweak_lsp_kind("prepend")
+
+require("resession").setup({
+  buf_filter = function(buffer)
+    -- Do not save any of the filetypes in this list with the session, defer to the default implementation for
+    -- everything else.
+    local skip_filetypes = {"gitcommit", "help"}
+    if vim.list_contains(skip_filetypes, vim.bo[buffer].filetype) then
+      return false
+    end
+
+    return require("resession").default_buf_filter(buffer)
+  end
+})
+
+require("telescope").load_extension("fzy_native")
+
+require("turnip").setup({
+  custom_groups = {
+    ["@keyword.doxygen"] = {fg = "green_faint"},
+    ["@keyword.modifier.doxygen"] = {link = "Comment"},
+    ["@punctuation.bracket.doxygen"] = {fg = "gray_slate"},
+  }
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {callback = function(_)
+  local resession = require("resession")
+  local lantern = require("lantern")
+  lantern.scan()
+  if lantern.project() ~= nil then
+    -- Try to load the session for this project, if it exists.
+    resession.load(lantern.project().name, {silence_errors = true})
+  end
+end})
+
+vim.api.nvim_create_autocmd("VimLeavePre", {callback = function(_)
+  -- If an active session exists, save it.
+  local resession = require("resession")
+  local session = resession.get_current_session_info()
+  if session ~= nil then
+    resession.save(session.name)
+  end
+end})
+
+vim.cmd("colorscheme turnip")
 
