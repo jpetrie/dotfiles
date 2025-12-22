8c5a7cc93ed2f2c03d106efdbb1d54eea258650f
Josh Petrie <josh@joshpetrie.net>
2025-12-22 12:42:41 -0800

    Improve handling of Lantern saved state.

--- home/dot_config/nvim/lua/resession/extensions/lantern.lua
+++ home/dot_config/nvim/lua/resession/extensions/lantern.lua
@@ -22,13 +22,19 @@ end
 
 M.on_post_load = function(state)
   local has_lantern, lantern = pcall(require, "lantern")
-  if has_lantern then
+  if has_lantern and lantern.project() ~= nil then
+    local project = lantern.project()
     if state.configuration ~= nil then
-      lantern.set_configuration(state.configuration)
+      if project.configurations[state.configuration] ~= nil then
+        lantern.set_configuration(state.configuration)
+      end
     end
 
-    if state.target ~= nil then
-      lantern.set_target(state.target)
+    if lantern.configuration() ~= nil and state.target ~= nil then
+      local configuration = lantern.configuration()
+      if configuration.targets[state.target] ~= nil then
+        lantern.set_target(state.target)
+      end
     end
   end
 end
