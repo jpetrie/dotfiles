fb267e84784c2735f0b7c20c7c420d0d163167a9
Josh Petrie <josh@joshpetrie.net>
2026-05-16 08:04:56 -0700

    Indicate what tsr is actually doing.

--- home/dot_config/fish/functions/tsr.fish
+++ home/dot_config/fish/functions/tsr.fish
@@ -1,13 +1,19 @@
 function tsr --description "Refresh Neovim Tree-sitter parsers and queries"
   for directory in (find "$CHEZMOI/nvim/parsers" -type d -depth 1)
     set -l language (string split --max 1 --right - (basename $directory))[-1]
-    tree-sitter build -o "$HOME/.config/nvim/parser/$language.so" $directory
+    set -l parser_output "$HOME/.config/nvim/parser/$language.so"
+    echo "Building $language parser to $parser_output"
+    tree-sitter build -o "$parser_output" $directory
   end
 
   for directory in (find "$CHEZMOI/nvim/queries" -type d -depth 1)
     set -l language (string split --max 1 --right - (basename $directory))[-1]
-    if not test -e "$HOME/.config/nvim/queries/$language"
-      ln -s $directory/queries "$HOME/.config/nvim/queries/$language"
+    set -l query_output "$HOME/.config/nvim/queries/$language"
+    if not test -e "$query_output"
+      echo "Linking $language queries to $query_output"
+      ln -s $directory/queries "$query_output"
+    else
+      echo "Skipping $language queries (already linked)"
     end
   end
 end
